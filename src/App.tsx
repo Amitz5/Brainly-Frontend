e39de6e08d1fb57540821e43a2ce7494f4ee957b
Amitz5 <amitozsingh475@gmail.com>
2025-07-25 04:59:04 +0530

    Creating Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { Button } from './Components/Button'
+import { PlusIcon } from './icons/Plusicon'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Button/>
+      <Button startIcon = {<PlusIcon size={"sm"}/>} size = "sm" variant ="primary" text ="share"/>
+      <Button size ="lg" variant ="secondary" text ="Add Content"/>
     </>
   )
 }
